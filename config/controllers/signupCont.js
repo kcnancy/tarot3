5552ab50db5f4f9d20f108d18b969a8b6840626e
Nancy McBride <njmcbride224@gmail.com>
2021-05-11 10:42:26 -0500

    adding config files

---
+++ config/controllers/signupCont.js
@@ -0,0 +1,25 @@
+// Requiring our models and passport as we've configured it
+const passport = require('../passport');
+const isAuthenticated = require('../middleware/isAuthenticated');
+// Import the model (user.js) to use its database functions.
+const db = require('../../models');
+
+module.exports = function (app) {
+
+  // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
+  // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
+  // otherwise send back an error
+  app.post('/api/signup', (req, res) => {
+    console.log(req.body);
+    db.User.create({
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(() => {
+        res.redirect(307, '/api/login');
+      })
+      .catch(err => {
+        res.status(401).json(err);
+      });
+  });
+};
---
+++ config/controllers/userdata.js
@@ -0,0 +1,51 @@
+// Requiring our models and passport as we've configured it
+const passport = require('../passport');
+const isAuthenticated = require('../middleware/isAuthenticated');
+// Import the model (user.js) to use its database functions.
+const db = require('../../models');
+
+module.exports = function (app) {
+  // Route for getting some data about our user to be used client side
+  app.get('/api/userdata', (req, res) => {
+    if (!req.user) {
+      // The user is not logged in, send back an empty object
+      res.json({});
+    } else {
+      // Otherwise send back the user's email and id
+      res.json({
+        email: req.user.email,
+        id: req.user.id
+      });
+    };
+  });
+
+  // route for deleting the user
+  app.delete('/api/user/:id', (req, res) => {
+    db.Users.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(dbUser => res.status(200).json({ msg: "user deleted" }))
+      .catch(err => res.status(500).json(err));
+  });
+
+  //route to get profile for the current loggedin user
+  app.get('/api/user/:id', (req, res) => {
+    db.Users.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [db.Profile, db.Post]
+    }).then((data) => res.status(200).json(data))
+      .catch(err => res.status(500).json(err))
+  });
+
+  //route to get all users with their profile and users
+  app.get('/api/user', (req, res) => {
+    db.Users.findAll({
+      include: [db.Profile, db.Post],
+    }).then(data => res.status(200).json(data))
+      .catch(err => res.status(500).json(err));
+  });
+
+};
