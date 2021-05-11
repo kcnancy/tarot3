af647982a2f3f4fb0a749b2864d2c5d47da3ce96
Nancy McBride <njmcbride224@gmail.com>
2021-05-11 10:43:34 -0500

    adding models

---
+++ models/User.js
@@ -0,0 +1,50 @@
+/* Requiring bcrypt for password hashing. Using the bcryptjs version as the regular
+bcrypt module sometimes causes errors on Windows machines */
+var bcrypt = require("bcryptjs");
+// Creating our Users model
+module.exports = function (sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    // The email cannot be null, and must be a proper email before creation
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    // The password cannot be null
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  /* Creating a custom method for our Users model. This will check if an un-hashed password
+  entered by the user can be compared to the hashed password stored in our database */
+  User.prototype.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+  // Hooks are automatic methods that run during various phases of the Users Model lifecycle
+  // In this case, before a Users is created, we will automatically hash their password
+  User.addHook("beforeCreate", function (user) {
+    user.password = bcrypt.hashSync(
+      user.password,
+      bcrypt.genSaltSync(10),
+      null
+    );
+  });
+  // Users.associate = (models) => {
+  //   // Associating Users with Posts
+  //   // When an User is deleted, also delete any associated Posts
+  //   Users.hasMany(models.Post, {
+  //     onDelete: "cascade",
+  //   });
+  //   // associating with profile
+  //   //when a user is deleted, also deletes the associated profile
+  //   Users.hasOne(models.Profile, {
+  //     onDelete: "cascade",
+  //   });
+  // };
+  return User;
+};
